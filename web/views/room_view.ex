a60012f89652420813307a2948bcb51065de03fe
Michał Warda <michal.warda@elpassion.pl>
2015-07-31 19:24:54 +0200

    Fix room rendering. For some weird reason it parses badly from new ecto version.

--- web/views/room_view.ex
+++ web/views/room_view.ex
@@ -2,6 +2,6 @@ defmodule PlanningPoker.RoomView do
   use PlanningPoker.Web, :view
 
   def render("create.json", %{room: room}) do
-    room
+    %{title: room.title, id: room.id, owner_id: room.owner_id}
   end
 end
