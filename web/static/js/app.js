c1063770536c6c3032f23be655d2b24b62fd6509
Michał Warda <michal.warda@elpassion.pl>
2015-08-05 11:30:29 +0200

    Change styles folder structure

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,18 +19,10 @@ var routes = (
   </Route>
 );
 
-console.log('start');
-
-// Router.run(routes, Router.HistoryLocation, function (Handler) {
-//   React.render(<Handler/>, document.body);
-// });
-//
 React.render(
   <Router history={new BrowserHistory()} children={routes}/>, document.body
 );
 
 let App = {};
-
-console.log('123');
 
 export default App;
