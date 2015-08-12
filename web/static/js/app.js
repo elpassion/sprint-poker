901df3e62bab50269aa71bf460919b29c248616b
Michał Kalbarczyk <fazibear@gmail.com>
2015-08-12 19:59:35 +0200

    cleanup

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -14,9 +14,9 @@ import NewGame from './components/NewGame';
 
 var routes = (
   <Route components={PlanningPokerReactApp}>
-    <Redirect from="/" to="new-room" />
-    <Route path="new-room" components={NewGame}/>
-    <Route path="rooms" components={Room}>
+    <Redirect from="/" to="new-game" />
+    <Route path="new-game" components={NewGame}/>
+    <Route path="games" components={Room}>
       <Route path="/:uuid" components={RoomLobby}/>
     </Route>
   </Route>
@@ -25,26 +25,3 @@ var routes = (
 React.render(
   <Router history={new BrowserHistory()} children={routes}/>, document.body
 );
-
-// import { Socket } from 'phoenix';
-//
-// var user = {name: "Silver Fo", id: "9f79514f-2c72-4bb3-930b-dd45528cad73", auth_token: "a383d3e5-e581-4a3f-9ab1-fe18052541e0"}
-//
-// var socket = new Socket("ws://localhost:4000/ws");
-// socket.connect(user);
-// socket.onOpen(ev => console.log("OPEN", ev));
-// socket.onError(ev => console.log("ERROR", ev));
-// socket.onClose(ev => console.log("CLOSE", ev));
-//
-// var channel = socket.channel('lobby', user)
-// channel
-//   .join()
-//   .receive("ignore", () => console.log("auth error"))
-//   .receive("ok", () =>
-//     channel.push("create_game", {name: "new game"})
-//   )
-//
-// channel.on("user", (user) => console.log("user", user))
-// channel.on("scales", (scales) => console.log("scales", scales))
-// channel.on("game", (game) => console.log("game", game))
-//
