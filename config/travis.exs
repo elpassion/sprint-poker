f257700b62d2542feea2ea6f7e2e6f22e5573d80
Michał Kalbarczyk <fazibear@gmail.com>
2015-08-14 11:57:18 +0200

    travis support

---
+++ config/travis.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :planning_poker, PlanningPoker.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :planning_poker, PlanningPoker.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "postgress",
+  pool: Ecto.Adapters.SQL.Sandbox
