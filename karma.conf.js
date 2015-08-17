cd0e248c7603a84d792e802f4df119f435542165
Michał Kalbarczyk <fazibear@gmail.com>
2015-08-17 11:07:56 +0200

    add testing fronend fraework

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+var webpackConfig = require('./webpack.config');
+webpackConfig.devtool = 'inline-source-map';
+
+module.exports = function (config) {
+  config.set({
+    plugins: [
+      'karma-jasmine',
+      'karma-sourcemap-loader',
+      'karma-webpack',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher'
+    ],
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: [ 'jasmine' ],
+    files: [
+      'test.webpack.js'
+    ],
+    preprocessors: {
+      'test.webpack.js': [ 'webpack', 'sourcemap' ]
+    },
+    reporters: [ 'dots' ],
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    }
+  });
+};
