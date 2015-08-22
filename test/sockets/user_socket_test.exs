65741c60902847f27c9c8bdecbc3ff3f7573d973
Michał Kalbarczyk <fazibear@gmail.com>
2015-08-23 00:02:08 +0200

    add user_socket test

---
+++ test/sockets/user_socket_test.exs
@@ -0,0 +1,21 @@
+defmodule PlanningPoker.UserSocketTest do
+  use PlanningPoker.ChannelCase
+
+  alias PlanningPoker.UserSocket
+  alias PlanningPoker.User
+
+  test "connecting to socket creates a new user" do
+    {:ok, socket} = UserSocket.connect(%{}, socket())
+
+    assert [user] = Repo.all(User)
+    assert socket.assigns.user_id == user.id
+  end
+
+  test "connecting to socket with auth_token assings this user" do
+    user = %User{} |> User.changeset(%{name: "test user"}) |> Repo.insert!
+    {:ok, socket} = UserSocket.connect(%{"auth_token" => user.auth_token}, socket())
+
+    assert socket.assigns.user_id == user.id
+  end
+
+end
