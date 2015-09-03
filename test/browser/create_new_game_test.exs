bfd5de918077a3d790dfb4530cbdce8357864054
Michał Kalbarczyk <fazibear@gmail.com>
2015-09-03 11:25:13 +0200

    CR fixes

--- test/browser/create_new_game_test.exs
+++ test/browser/create_new_game_test.exs
@@ -4,24 +4,24 @@ defmodule PlanningPoker.CreateNewGameTest do
   test "don't create game without session and user name" do
     visit "/"
     click_button "Start Session"
-    assert Page.has_text? "Session Title Cant be blank"
-    assert Page.has_text? "Your Nickname Cant be blank"
+    assert Page.has_text? "Session Title can't be blank"
+    assert Page.has_text? "Your Nickname can't be blank"
   end
 
   test "don't create game without session name" do
     visit "/"
     fill_in "game_name", "Game"
     click_button "Start Session"
-    refute Page.has_text? "Session Title Cant be blank"
-    assert Page.has_text? "Your Nickname Cant be blank"
+    refute Page.has_text? "Session Title can't be blank"
+    assert Page.has_text? "Your Nickname can't be blank"
   end
 
   test "don't create game without user name" do
     visit "/"
     fill_in "user_name", "User"
     click_button "Start Session"
-    assert Page.has_text? "Session Title Cant be blank"
-    refute Page.has_text? "Your Nickname Cant be blank"
+    assert Page.has_text? "Session Title can't be blank"
+    refute Page.has_text? "Your Nickname can't be blank"
   end
 
   test "create game with session and user name" do
@@ -30,8 +30,8 @@ defmodule PlanningPoker.CreateNewGameTest do
     fill_in "user_name", "Test User"
     select "T-Shirts (S, M, L, XXL)", from: "game_deck"
     click_button "Start Session"
-    refute Page.has_text? "Session Title Cant be blank"
-    refute Page.has_text? "Your Nickname Cant be blank"
+    refute Page.has_text? "Session Title can't be blank"
+    refute Page.has_text? "Your Nickname can't be blank"
     assert Page.has_text? "Test Game"
     assert Page.has_text? "Test User"
   end
