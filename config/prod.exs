8fb979f22ee325a0a5c4540fd2b4dbb0cc0244b1
Michał Kalbarczyk <fazibear@gmail.com>
2015-09-11 16:11:30 +0200

    move host to system variable

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :planning_poker, PlanningPoker.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "planning-poker-phoenix.herokuapp.com", port: 80],
+  url: [host: System.get_env("HOST"), port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 config :planning_poker, PlanningPoker.Endpoint,
