fd1c6c72040a9219deffc7f7bef00e323b232518
Michał Kalbarczyk <fazibear@gmail.com>
2016-03-24 13:43:51 +0100

    cast id to string

--- web/serializers/state_serializer.ex
+++ web/serializers/state_serializer.ex
@@ -1,8 +1,12 @@
 defimpl Poison.Encoder, for: SprintPoker.State do
   def encode(state, options) do
+    current_ticket_id = case state.current_ticket_id do
+      nil -> nil
+      id -> Integer.to_string(id)
+    end
     %{
       name: state.name,
-      current_ticket_id: state.current_ticket_id,
+      current_ticket_id: current_ticket_id,
       votes: state.votes
     } |> Poison.Encoder.encode(options)
   end
