8b3114096151708b82e13fbcf11431a54d0ffe57
Michał Kalbarczyk <fazibear@gmail.com>
2017-08-02 12:26:34 +0200

    Update phoenix

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule SprintPoker.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
